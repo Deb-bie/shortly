b830b46338f51302688c3cedc5228ab8d4d2ad8e
Debbie <asamoahjaeldeborah@gmail.com>
2022-11-21 11:36:44 +0000

    Changes

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -85,7 +85,7 @@ const Search = () => {
                             <div key={id} className=" w-[100%] h-[100%] py-6 px-4 gap-y-4 flex 4xs:flex-col sm:flex-row justify-between content-center items-center bg-white shadow-2xl " >
                                 <span className="4xs:w-[100%] sm:w-[40%] inline-block overflow-hidden text-ellips ">{item.origUrl}</span>
 
-                                <a href={item.origUrl} target="_blank" className="sm:w-[30%] 4xs:w-[100%] inline-block overflow-hidden text-ellipsis text-blue-800 underline ">https://shortly-api-b5u2.onrender.com/{item.shortUrl}</a>
+                                <a href={item.origUrl} target="_blank" className="sm:w-[30%] 4xs:w-[100%] inline-block overflow-hidden text-ellipsis text-blue-800 underline ">{item.mya}</a>
 
                                 <div className="4xs:w-[100%] sm:w-[20%] gap-y-4 flex flex-col justify-between ">
                                     {/* <button className="px-4  py-2 bg-[#d3f3f3] text-white outline-none rounded-[5px] ">Copy</button> */}
